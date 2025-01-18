032a7a8918712f775406170e2bc8ff4a1c37587a
Rostislav <rostik057@gmail.com>
2025-01-18 23:42:53 +0300

    !fix category

--- components/Category/Item.vue
+++ components/Category/Item.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="bg-white rounded-xl flex flex-col items-center p-6 h-full">
+  <div
+    class="category-item bg-white rounded-xl relative flex flex-col items-center p-6 h-full before:shadow-md before:duration-300"
+  >
     <div class="w-4/5">
       <div class="flex relative pt-[100%] mb-3 w-full">
         <img
@@ -23,3 +25,24 @@ interface IProps {
 
 const props = defineProps<IProps>();
 </script>
+
+<style lang="scss" scoped>
+.category-item {
+  &::before {
+    border-radius: inherit;
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    opacity: 0;
+    width: 100%;
+    height: 100%;
+  }
+
+  &:hover {
+    &::before {
+      opacity: 100%;
+    }
+  }
+}
+</style>
